fdb7ae88866e2846278259500af5e0e9e6e6474d
jbsalameh <jbsalameh@gmail.com>
2026-03-02 12:05:43 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1444,13 +1444,3 @@ function PlanPreview({ plan, setPlan, onAccept, onDiscard }) {
     </div>
   );
 }
-
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
